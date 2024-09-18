09111440aaea825b1e441c705e7d78453dc4469b
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-09-18 14:55:40 +0700

    chore: delete frontend folder & files

--- frontend/libs/tsr-react.query.ts
+++
@@ -1,8 +0,0 @@
-// import { initTsrReactQuery } from "@ts-rest/react-query/v5";
-
-// // TODO: Import Contracts
-
-// export const api = initTsrReactQuery(contract, {
-//   baseUrl: process.env.BACKEND_BASE_URL,
-//   baseHeaders: {},
-// });
