e23748d3d30761a35b2733eae94200d559b41cba
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-09-24 18:12:51 +0700

    feat(backend): add Task model for testing

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,4 @@
-from .models import Task
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register(Task)
+# admin.site.register(Task)
--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,20 @@ from datetime import timezone
 from django.db import models
 
 
+class Task(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-date_created']
+        db_table = 'task'
+
+    def __str__(self):
+        return self.title
+
+
 class MyBaseModel(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
