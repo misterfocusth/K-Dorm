64017fb8cef74cc95cfa2b3a6877ba8ef0082853
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-09-30 12:38:25 +0700

    feat(backend): Add Google login API

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -4,6 +4,8 @@ from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
 
+from .views import GoogleLogin
+
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
@@ -11,4 +13,5 @@ urlpatterns = [
     path("user/", UserDetailsView.as_view(), name="rest_user_details"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
+    path("google/", GoogleLogin.as_view(), name="google_login"),
 ]
