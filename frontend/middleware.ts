cb7cde20d0e803ec76e55e33f62f83c051b60abc
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-10-01 21:42:59 +0700

    feat(frontend): middleware to handle authentication redirects

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,20 +1,33 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { STUDENT_HOME_ROUTE, ROOT_ROUTE, SESSION_UID_COOKIE_NAME } from "./constants";
+import {
+  STUDENT_HOME_ROUTE,
+  ROOT_ROUTE,
+  SESSION_UID_COOKIE_NAME,
+  SESSION_ID_TOKEN_COOKIE_NAME,
+  STUDENT_LOGIN_ROUTE,
+  STAFF_LOGIN_ROUTE,
+} from "./constants";
 
 const protectedRoutes = [STUDENT_HOME_ROUTE];
 
 export default function middleware(request: NextRequest) {
-  const session = request.cookies.get(SESSION_UID_COOKIE_NAME)?.value || "";
+  const session = request.cookies.get(SESSION_ID_TOKEN_COOKIE_NAME)?.value || "";
+  const uid = request.cookies.get(SESSION_UID_COOKIE_NAME)?.value || "";
 
-  // Redirect to login if session is not set
-  if (!session && protectedRoutes.includes(request.nextUrl.pathname)) {
-    const absoluteURL = new URL(ROOT_ROUTE, request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
-  }
+  const isLoggedIn = !!session && !!uid;
 
-  // Redirect to home if session is set and user tries to access root
-  if (session && request.nextUrl.pathname === ROOT_ROUTE) {
-    const absoluteURL = new URL(STUDENT_HOME_ROUTE, request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
+  const prefixRequestUrl = request.nextUrl.pathname.split("/")[1];
+  const isLogInPath = request.nextUrl.pathname.endsWith("login");
+
+  if (!isLoggedIn) {
+    if (prefixRequestUrl === "student" && !isLogInPath) {
+      const absoluteURL = new URL(STUDENT_LOGIN_ROUTE, request.nextUrl.origin);
+      return NextResponse.redirect(absoluteURL.toString());
+    }
+
+    if (prefixRequestUrl === "staff" && !isLogInPath) {
+      const absoluteURL = new URL(STAFF_LOGIN_ROUTE, request.nextUrl.origin);
+      return NextResponse.redirect(absoluteURL.toString());
+    }
   }
 }
