0aea8728544aad1f5b5bb6a8147734f33b058c8b
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-10-03 00:04:42 +0700

    feat(frontend): middleware handle if isLoggedIn but trying to access login page

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -6,6 +6,8 @@ import {
   STAFF_LOGIN_ROUTE,
   STUDENT_ROUTE_PREFIX,
   STAFF_ROUTE_PREFIX,
+  STUDENT_HOME_ROUTE,
+  STAFF_HOME_ROUTE,
 } from "./constants";
 
 const PUBLIC_STUDENT_ROUTES = ["login", "onboarding"].map(
@@ -32,6 +34,21 @@ export default function middleware(request: NextRequest) {
 
     if (prefixRequestUrl === "staff" && !isPubicRoute) {
       const absoluteURL = new URL(STAFF_LOGIN_ROUTE, request.nextUrl.origin);
+      return NextResponse.redirect(absoluteURL.toString());
+    }
+  }
+
+  // Handle if isLoggedIn but trying to access login page
+  const isLoginPage = request.nextUrl.pathname.includes("login");
+
+  if (isLoggedIn && isLoginPage) {
+    if (prefixRequestUrl === "student") {
+      const absoluteURL = new URL(STUDENT_HOME_ROUTE, request.nextUrl.origin);
+      return NextResponse.redirect(absoluteURL.toString());
+    }
+
+    if (prefixRequestUrl === "staff") {
+      const absoluteURL = new URL(STAFF_HOME_ROUTE, request.nextUrl.origin);
       return NextResponse.redirect(absoluteURL.toString());
     }
   }
