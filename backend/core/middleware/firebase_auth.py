b70d897efe7482409d59332c987333921edd8706
DrowningToast <supratouchgussii@gmail.com>
2024-10-14 21:45:23 +0700

    fix:

--- backend/core/middleware/firebase_auth.py
+++ backend/core/middleware/firebase_auth.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 
 from firebase_admin import auth
 
-from backend.interfaces.request_with_context import RequestWithContext
+from interfaces.request_with_context import RequestWithContext
 from repositories.account import AccountRepository
 
 
