0820aee0e9a45c4a00c005a26bd8392315d17575
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-10-17 03:24:21 +0700

    feat: activity category backend

---
+++ backend/api/repository/activity_category.py
@@ -0,0 +1,78 @@
+# Model
+from typing import NotRequired, Optional, TypedDict
+from exception.application_logic.client.not_found import NotFoundException
+from exception.application_logic.server.Illegal_operation import (
+    IllegalOperationException,
+)
+from domain.models import ActivityCategory, Student, Staff
+
+from django.db.models import Model
+from exception.base_stackable_exception import StackableException
+from exception.unknown_exception import UnknownException
+
+
+class CreateActivityCategoryPayload(TypedDict):
+    handle: str
+    name: str
+    visibleToStudents: bool
+    visibleToStaffs: bool
+    visibleToSecurityStaffs: bool
+
+
+class EditActivityCategoryPayload(TypedDict):
+    handle: NotRequired[str]
+    name: NotRequired[str]
+    visibleToStudents: NotRequired[bool]
+    visibleToStaffs: NotRequired[bool]
+    visibleToSecurityStaffs: NotRequired[bool]
+
+
+class ActivityCategoryRepository:
+
+    @staticmethod
+    def get_all() -> list[ActivityCategory]:
+        try:
+            activity_categories = ActivityCategory.objects.all()
+            return list(activity_categories)
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def create(
+        handle: str,
+        name: str,
+        visibleToStudents: bool,
+        visibleToStaffs: bool,
+        visibleToSecurityStaffs: bool,
+    ):
+        activity_category = ActivityCategory.objects.create(
+            handle=handle,
+            name=name,
+            visibleToStudents=visibleToStudents,
+            visibleToStaffs=visibleToStaffs,
+            visibleToSecurityStaffs=visibleToSecurityStaffs,
+        )
+        activity_category.save()
+
+        return activity_category
+
+    @staticmethod
+    def edit_by_id(
+        activity_category_id: str,
+        payload: EditActivityCategoryPayload
+    ) -> ActivityCategory:
+        try:
+            activity_category = ActivityCategory.objects.get(
+                id=activity_category_id)
+
+            for key, value in payload.items():
+                if value is not None:
+                    setattr(activity_category, key, value)
+
+            activity_category.save()
+
+            return activity_category
+        except ActivityCategory.DoesNotExist:
+            raise NotFoundException("ActivityCategory not found")
+        except Exception as e:
+            raise e
---
+++ backend/api/use_case/activity_category/activity_category_uc.py
@@ -0,0 +1,57 @@
+from time import clock_getres
+from typing import List, Optional, TypedDict
+from api.repository.room_repository import RoomRepository
+from domain.models import Room
+from exception.application_logic.client.not_found import NotFoundException
+from exception.unknown_exception import UnknownException
+from interfaces.context import Context
+from interfaces.request_with_context import RequestWithContext
+from layer.handle import handle
+from layer.use_case import usecase
+from api.repository.activity_category import ActivityCategoryRepository
+from typing import NotRequired, Optional, TypedDict
+
+
+class EditActivityCategoryPayload(TypedDict):
+    handle: NotRequired[str]
+    name: NotRequired[str]
+    visibleToStudents: NotRequired[bool]
+    visibleToStaffs: NotRequired[bool]
+    visibleToSecurityStaffs: NotRequired[bool]
+
+
+class CreateActivityCategoryPayload(TypedDict):
+    handle: str
+    name: str
+    visibleToStudents: bool
+    visibleToStaffs: bool
+    visibleToSecurityStaffs: bool
+
+
+@usecase(only_authenticated=True)
+def get_list(ctx: Context):
+    activity_categories = ActivityCategoryRepository.get_all()
+    return activity_categories
+
+
+@usecase(
+    only_authenticated=True,
+)
+def create(ctx: Context, payload: CreateActivityCategoryPayload):
+    activity_category = ActivityCategoryRepository.create(
+        payload["handle"],
+        payload["name"],
+        payload["visibleToStudents"],
+        payload["visibleToStaffs"],
+        payload["visibleToSecurityStaffs"],
+    )
+    return activity_category
+
+
+@usecase(
+    only_authenticated=True,
+)
+def edit_by_id(ctx: Context, activity_category_id: str, payload: EditActivityCategoryPayload):
+    activity_category = ActivityCategoryRepository.edit_by_id(
+        activity_category_id, payload)
+    return activity_category
